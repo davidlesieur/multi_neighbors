f91de57e500f0a1f6335a8ce9d3e9383884931c7
David Lesieur <david@davidlesieur.com>
2018-07-13 14:03:30 -0400

    Simplify logic.

--- multi_neighbors.py
+++ multi_neighbors.py
@@ -4,32 +4,14 @@ This plugin adds ``next_articles`` (newer) and ``prev_articles`` (older)
 variables to every article's context.
 """
 
-from collections import deque
 from pelican import signals
 
 
 def neighbors(generator):
-    # Populate prev_articles.
-    prevs = deque([], generator.settings.get('MULTI_NEIGHBORS', 5))
-    for article in reversed(generator.articles):
-        if not hasattr(article, 'prev_articles'):
-            article.prev_articles = []
-        if prevs:
-            article.prev_articles.extend(prevs)
-            if len(prevs) == prevs.maxlen:
-                prevs.pop()
-        prevs.appendleft(article)
-
-    # Populate next_articles.
-    nexts = deque([], generator.settings.get('MULTI_NEIGHBORS', 5))
-    for article in generator.articles:
-        if not hasattr(article, 'next_articles'):
-            article.next_articles = []
-        if nexts:
-            article.next_articles.extend(nexts)
-            if len(nexts) == nexts.maxlen:
-                nexts.pop()
-        nexts.appendleft(article)
+    n = generator.settings.get('MULTI_NEIGHBORS', 5)
+    for i, article in enumerate(generator.articles):
+        article.prev_articles = generator.articles[min(i+1, len(generator.articles)):i+1+n]
+        article.next_articles = list(reversed(generator.articles[max(0,i-n):i]))
 
 
 def register():
